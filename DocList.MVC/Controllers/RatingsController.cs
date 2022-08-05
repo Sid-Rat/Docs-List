e058ed97a5e2f14259c66508017ead7bd31bf478
Tim Huser <sidratgames@protonmail.com>
2022-08-05 11:31:49 -0400

    verifing push

---
+++ DocList.MVC/Controllers/RatingsController.cs
@@ -0,0 +1,92 @@
+﻿using DocList.Models.Ratings;
+using DocList.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocList.Controllers
+{
+    public class RatingsController : Controller
+
+
+    {
+        private readonly IRatingsService _ratings;
+        public RatingsController(IRatingsService ratingService)
+        {
+            _ratings = ratingService;
+        }
+
+        // GET: RatingsController
+        public IActionResult Index()
+        {
+            var getRating = _ratings.GetRatings();
+            return View(getRating);
+        }
+
+        // GET: RatingsController/Details/5
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: RatingsController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RatingsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(RatingListItem model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            if (_ratings.Rating(model))
+                return RedirectToAction("Index");
+
+            return View(model);
+        }
+
+        // GET: RatingsController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RatingsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RatingsController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RatingsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DocList.Services/IRatingsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocList.Models;
+using DocList.Models.Ratings;
+
+namespace DocList.Services
+{
+    public interface IRatingsService
+    {
+        bool CreateRating(RatingListItem rating);
+        IEnumerable<RatingListItem> GetRatings();
+        RatingListItem GetRating(int id);
+        RatingListItem GetRating(string id);
+        RatingListItem GetRating(RatingListItem rating);
+        bool Rating(RatingListItem model);
+    }
+}
